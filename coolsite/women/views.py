52254461d1c2ad2c7111555c232b9cadfa3616fe
Deriabin E <Deriabin1985@gmail.com>
2023-02-26 17:38:53 +0300

    create urls in templates

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -2,18 +2,41 @@ from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 from .models import *
 
-menu = ['about site', 'add article', 'callback', 'Enter']
+menu = [{'title': 'About site', 'url_name': 'about'},
+        {'title': 'Add article', 'url_name': 'add_page'},
+        {'title': 'Callback', 'url_name': 'contact'},
+        {'title': 'Enter', 'url_name': 'login'}
+        ]
 
 
 def index(request):
     posts = Women.objects.all()
-    return render(request, 'women/index.html', {'posts': posts, 'menu': menu, 'title': 'Main page'})
+    context = {
+        'posts': posts,
+        'menu': menu,
+        'title': 'Main page'
+    }
+    return render(request, 'women/index.html', context=context)
 
 
 def about(request):
     return render(request, 'women/about.html', {'menu': menu, 'title': 'About site'})
 
 
+def addpage(request):
+    return HttpResponse('add article')
+
+
+def contact(request):
+    return HttpResponse('contacts')
+
+
+def login(request):
+    return HttpResponse('authorization')
+
+
+def show_post(request, post_id):
+    return HttpResponse(f'display article id = {post_id}')
 def categories(request, cat):
     if request.GET:
         print(request.GET)
