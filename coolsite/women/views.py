f8af77af5c9697d1d648ce1b92bf7ac0c2fd5600
Deriabin E <Deriabin1985@gmail.com>
2023-02-27 11:18:04 +0300

    rebase db, add slug to urls

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import *
 
 
@@ -29,15 +29,25 @@ def login(request):
     return HttpResponse('authorization')
 
 
-def show_post(request, post_id):
-    return HttpResponse(f'display article id = {post_id}')
+def show_post(request, post_slug):
+    post = get_object_or_404(Women, slug=post_slug)
+    context = {
+        'post': post,
+        'title': post.title,
+        'cat_selected': post.cat_id
+    }
+    return render(request, 'women/post.html', context=context)
 
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Page not found</h1>')
 
 
-def show_category(request, cat_id):
+def show_category(request, cat_slug):
+    if cat_slug == 'singers':
+        cat_id = 2
+    else:
+        cat_id = 1
     posts = Women.objects.filter(cat_id=cat_id)
 
     if len(posts) == 0:
